3a5645334a07d3f1217dfb09a6f8be9a9b1ed6a1
RavinKumar <ravinsdrive@gmail.com>
2023-10-07 10:54:00 -0700

    Run black on tests for nicer formatting

--- tests/test_chat/test_model.py
+++ tests/test_chat/test_model.py
@@ -5,8 +5,14 @@ from uuid import uuid4
 
 url = "http://llm_router:8000"
 
+
 def generate_for_model(model: str):
-    payload = {"uuid": str(uuid4()), "prompt": "test", "preprompt": "test", "model": model}
+    payload = {
+        "uuid": str(uuid4()),
+        "prompt": "test",
+        "preprompt": "test",
+        "model": model,
+    }
     response = requests.post(url + "/chat/generate", json=payload)
     assert response.status_code == 200
     generation = response.json()["generation"]
@@ -14,15 +20,23 @@ def generate_for_model(model: str):
 
     history = [{"prompt": "test", "generation": generation}]
 
-    payload = {"uuid": str(uuid4()), "prompt": "test", "preprompt": "test", "model": model, "history": history}
+    payload = {
+        "uuid": str(uuid4()),
+        "prompt": "test",
+        "preprompt": "test",
+        "model": model,
+        "history": history,
+    }
     response = requests.post(url + "/chat/generate", json=payload)
     assert response.status_code == 200
     generation = response.json()["generation"]
     assert generation != ""
 
+
 @pytest.mark.external
 def test_generate_huggingface():
     generate_for_model("oasst-pythia-12b")
+
 
 @pytest.mark.external
 def test_generate_openai():
