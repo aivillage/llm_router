2551712eae4cec17ca943d6502d636c364ab220e
Sven Cattell <sven@nbhd.ai>
2025-02-19 16:23:50 -0500

    ??

--- kube/create_cluster.sh
+++ kube/create_cluster.sh
@@ -2,12 +2,12 @@
 
 gcloud config set project hoth-410100 
 export PROJECT_ID=$(gcloud config get project)
-export REGION=asia-east1
+export REGION=asia-east1-a
 
-gcloud container clusters create hoth-demo --location ${REGION} \
+gcloud container clusters create hoth-demo-1 --location ${REGION} \
   --workload-pool ${PROJECT_ID}.svc.id.goog \
   --enable-image-streaming \
-  --node-locations=$REGION-a \
+  --node-locations=$REGION \
   --workload-pool=${PROJECT_ID}.svc.id.goog \
   --addons GcsFuseCsiDriver   \
   --machine-type n2d-standard-4 \
@@ -20,5 +20,5 @@ gcloud container node-pools create p100-test --cluster hoth-demo \
   --machine-type n1-standard-8 \
   --ephemeral-storage-local-ssd=count=1 \
   --enable-autoscaling --enable-image-streaming \
-  --num-nodes=0 --min-nodes=0 --max-nodes=20 \
-  --node-locations $REGION-a,$REGION-c --region $REGION --spot
+  --num-nodes=0 --min-nodes=0 --max-nodes=4 \
+  --node-locations $REGION --region $REGION --spot
